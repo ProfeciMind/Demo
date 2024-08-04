962a6d5d912d2497fef7ea9d9ac923ba0d06b9dd
Profeci <harshkumar2002jha@gmail.com>
2024-08-05 03:23:59 +0530

    create signup api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
-
+import userRouter from "./routes/user.route.js"
+import authRouter from "./routes/auth.route.js"
 const mongoURI="mongodb+srv://harsh:harsh@mern-estate.huxd3yr.mongodb.net/mern_estate";
 
 const connectToMongo = async () => {
@@ -18,7 +19,12 @@ const connectToMongo = async () => {
 connectToMongo();
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000');  
 }
 );
+
+app.use("/api/user",userRouter);
+app.use("/api/auth",authRouter);
