ca80b16a943088f70a3b4be708fa3ee1881ae371
Profeci <harshkumar2002jha@gmail.com>
2024-08-07 03:43:55 +0530

    Complete signpu functunality handling middleware and possible errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,17 +1,21 @@
+ import { errorHandler } from "../error.js";
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs'
 
 
-export const signup = async (req,res)=>{
+export const signup = async (req,res,next)=>{
+console.log("Reach");
 
-    const {username,email,password}= req.body;
-    const hashedPassword = bcryptjs.hashSync(password,10);
+    const {username,email,passwprd}= req.body;
+    console.log(req.body);
+    
+    const hashedPassword = bcryptjs.hashSync(passwprd,10);
     const newUser=new User({username,email,password:hashedPassword});
     try{
 
     await newUser.save();
     res.status(201).json("User created successfully")
     }catch(error){
-        res.status(500).json(error.message);
+        next(error);
     }
 };
--- api/index.js
+++ api/index.js
@@ -2,14 +2,14 @@ import express from 'express';
 import mongoose from 'mongoose';
 import userRouter from "./routes/user.route.js"
 import authRouter from "./routes/auth.route.js"
-const mongoURI="mongodb+srv://harsh:harsh@mern-estate.huxd3yr.mongodb.net/mern_estate";
+const mongoURI = "mongodb+srv://harsh:harsh@mern-estate.huxd3yr.mongodb.net/mern_estate";
 
 const connectToMongo = async () => {
     try {
-       await mongoose.connect(mongoURI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-     });
+        await mongoose.connect(mongoURI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
         console.log("[+] Connected to MongoDB Successfully");
     }
     catch (error) {
@@ -22,9 +22,19 @@ const app = express();
 app.use(express.json());
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');  
+    console.log('Server is running on port 3000');
 }
 );
 
-app.use("/api/user",userRouter);
-app.use("/api/auth",authRouter);
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal server Error';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
